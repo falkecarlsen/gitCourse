1b0fa23238e0dd6cccab02057d4afce3beb4bce1
cogitantium <falke.cs@gmail.com>
2017-11-15 23:25:06 +0100

    added readability to prints and utilized stdlib constants

--- vectors.c
+++ vectors.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct vector{
     double x;
@@ -14,24 +14,24 @@ void vector_print(vector a);
 int main(void) {
 
     vector a, b, c, d;
-    
+
     a.x = 5;
     a.y = 4;
-    
+
     b.x = 1;
     b.y = 6;
-    
+
     c = vector_add(a, b);
-    
+
     d = vector_scale(a, 3);
-    
+
     printf("a: "); vector_print(a);
     printf("\nb: "); vector_print(b);
-    printf("\nc: "); vector_print(c);
-    printf("\nd: "); vector_print(d);
+    printf("\na add b: "); vector_print(c);
+    printf("\na scaled by 3: "); vector_print(d);
     printf("\na dot c: %f\n", vector_dot(a, c));
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 double vector_dot(vector vector_1, vector vector_2){
