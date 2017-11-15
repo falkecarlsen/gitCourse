1951c14bf078b0ce46ffaf1331c38e7d6b5a17e4
cogitantium <falke.cs@gmail.com>
2017-11-15 23:31:53 +0100

    cleaned up variable names and defined a constant

--- vectors.c
+++ vectors.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define VECTORSCALE 3
+
 typedef struct vector{
     double x;
     double y;
@@ -13,7 +15,7 @@ void vector_print(vector a);
 
 int main(void) {
 
-    vector a, b, c, d;
+    vector a, b, vectorAdd, vectorScale;
 
     a.x = 5;
     a.y = 4;
@@ -21,15 +23,15 @@ int main(void) {
     b.x = 1;
     b.y = 6;
 
-    c = vector_add(a, b);
+    vectorAdd = vector_add(a, b);
 
-    d = vector_scale(a, 3);
+    vectorScale = vector_scale(a, VECTORSCALE);
 
     printf("a: "); vector_print(a);
     printf("\nb: "); vector_print(b);
-    printf("\na add b: "); vector_print(c);
-    printf("\na scaled by 3: "); vector_print(d);
-    printf("\na dot c: %f\n", vector_dot(a, c));
+    printf("\na add b: "); vector_print(vectorAdd);
+    printf("\na scaled by 3: "); vector_print(vectorScale);
+    printf("\na dot c: %f\n", vector_dot(a, vectorAdd));
 
     return EXIT_SUCCESS;
 }
