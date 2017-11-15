4d8bbbb29c522f27b4a55638278d672c1ae0e90e
NicEastvillage <nicoesterby@gmail.com>
2017-11-15 13:24:53 +0100

    declaring vectors in main and calling funtions

--- vectors.c
+++ vectors.c
@@ -3,6 +3,25 @@
 
 int main(void) {
     
+    vector a, b, c, d;
+    double dot, mag;
+    
+    a.x = 5;
+    a.y = 4;
+    
+    b.x = 1;
+    b.y = -2;
+    
+    c = vector_scale(a, 3);
+    d = vector_add(a, b);
+    dot = vector_dot(a, c);
+    
+    printf("a: "); vector_print(a);
+    printf("b: "); vector_print(b);
+    printf("c: "); vector_print(c);
+    printf("d: "); vector_print(d);
+    printf("a dot b: %d\n", dot);
+    
     return 0;
 }
 
