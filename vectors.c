e9724a27b13b3fc374ee1adf3d47962d62cb09a5
Mathias Sørensen <mmsa17@student.aau.dk>
2017-11-15 13:25:22 +0100

    made dotfunction

--- vectors.c
+++ vectors.c
@@ -1,7 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+double dotproduct(vector vector_1, vector vector_2);
+
 int main(void) {
     
+
     return 0;
+}
+
+double dotproduct(vector vector_1, vector vector_2){
+    double result;
+    result = vector_1.x * vector_2.x + vector_1.y * vector_2.y;
+    return result;
 }
